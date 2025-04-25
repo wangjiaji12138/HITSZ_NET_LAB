e8b300ecf6ef829a95b3a564b64580c445d8b2d8
CardiJ <wangjiaji386@gmail.com>
2025-04-25 21:34:22 +0800

    lab2 finish

--- src/ethernet.c
+++ src/ethernet.c
@@ -11,6 +11,18 @@
  */
 void ethernet_in(buf_t *buf) {
     // TO-DO
+    // step1
+    if(buf->len < sizeof(ether_hdr_t)){
+        return;
+    }
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    uint16_t protocol = swap16(hdr->protocol16);
+    // step2
+    buf_remove_header(buf,sizeof(ether_hdr_t));
+
+    // step3
+    net_in(buf,protocol,hdr->src);
+
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -21,6 +33,26 @@ void ethernet_in(buf_t *buf) {
  */
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol) {
     // TO-DO
+    // step1
+    if(buf->len < ETHERNET_MIN_TRANSPORT_UNIT){
+        buf_add_padding(buf,ETHERNET_MIN_TRANSPORT_UNIT-buf->len);
+    }
+    // step2
+    buf_add_header(buf, sizeof(ether_hdr_t));
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+    // step3
+    memcpy(hdr->dst,mac,NET_MAC_LEN);
+
+    //step4
+    memcpy(hdr->src,net_if_mac,NET_MAC_LEN);
+
+    //step5
+    hdr->protocol16=swap16(protocol);
+
+    //step6
+    driver_send(buf);
+
 }
 /**
  * @brief 初始化以太网协议
